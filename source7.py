a3db1a1b2b2e27d717beb4b1a68a33690e82593b
jBrewing <jbrewer256@gmail.com>
2018-11-13 17:29:23 -0700

    DONE.

--- source7.py
+++ source7.py
@@ -16,6 +16,7 @@ import datetime
 from suds.client import Client
 from pandas import Series
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # 2. connect to website
 # Get time 'now' to bring data to present
@@ -38,7 +39,7 @@ response = NWIS.GetValuesObject(siteCode, variableCode, beginDate, endDate)
 # Get the site's name from the response
 siteName = response.timeSeries[0].sourceInfo.siteName
 
-
+# 3. Load called objects into pandas series
 # Create some blank lists in which to put the values and their dates
 a = []  # The values
 b = []  # The dates
@@ -55,28 +56,39 @@ for v in values:
 # Set the index of the Series object to the dates
 ts = Series(a, index=b)
 
+# 4. Resample data on daily interval with mean/min/max functions
 # create hourly max, min, and avg series.
-hourlyTotDis = ts.resample(rule='1H', base=0).sum()
-hourlyAvgVol = hourlyTotDis.mean()
+hourlyTotDisAvg = ts.resample(rule='1D', base=0).mean()
+hourlyTotDisMax = ts.resample(rule='1D', base=0).max()
+hourlyTotDisMin = ts.resample(rule='1D', base=0).min()
 
+
+#  5. Plot stuff
 # Use MatPlotLib to create a plot of the time series
 # Create a plot of the streamflow statistics
 # ------------------------------------------
 # Create a figure object and add a subplot
-fig = plt.figure()
+fig = plt.figure(figsize=(12,8))
 ax = fig.add_subplot(1, 1, 1)  # arguments for add_subplot - add_subplot(nrows, ncols, plot_number)
 
 # Call the plot() methods on the series object to plot the data
-ts.plot(color='blue', linestyle='solid', label='15-minute streamflow values')
-
+ts.plot(color='grey', linestyle='solid', label='15-minute streamflow values', alpha=0.5, linewidth=0.5)
+hourlyTotDisAvg.plot(color='green', linestyle='solid', label='Daily avg flows',
+                     marker = 'o', ms=2, linewidth =0.75)
+hourlyTotDisMax.plot(color='red', linestyle='solid', label='Daily max flows',
+                     marker = 'o', ms=2, linewidth =0.75)
+hourlyTotDisMin.plot(color='blue', linestyle='solid', label='Daily min flows',
+                     marker = 'o', ms=2, linewidth =0.75)
 # Set some properties of the subplot to make it look nice
 ax.set_ylabel('Discharge, cubic feet per second')
 ax.set_xlabel('Date')
 ax.grid(True)
-ax.set_title(siteName)
-
+ax.set_title('Daily Max, Min, & Avg Flows for: ' + siteName + ', ' + siteCode)
+ax.set_xlim(beginDate, endDate) #set limits with date variables
 # Add a legend with some customizations
 legend = ax.legend(loc='upper left', shadow=True)
+fig.autofmt_xdate()  # use auto-formatter to enable accurate date representation with mouse
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=15))  # set ticks interval for every 15 days.
 
 # Create a frame around the legend.
 frame = legend.get_frame()
